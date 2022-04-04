3f10ca76f68bbf6a159b9515b7b6c9467c60b94a
lukeeboy-coding <luke.crawford12356@gmail.com>
2022-04-04 02:12:31 +0000

    addedfile

---
+++ LearningPointers.c
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+struct employee
+{
+    char name[4];
+    int age;
+    float weight;
+};
+
+void fun(struct employee*);
+
+int main()
+{
+    struct employee e = {"abc", 20, 55.5};
+    fun(&e);
+
+    return 0;
+}
+
+void fun(struct employee *obj)
+{
+
+}
+
+int *ptr;
+char *ptr;
+float *ptr;
+double *ptr; 
