f46646fc62a392e997cc491ec096ea40e75e3188
lukeeboy-coding <luke.crawford12356@gmail.com>
2022-04-06 17:40:31 +0000

    lolol

---
+++ Untitled-1.md
@@ -0,0 +1,10 @@
+## A list of my teachers (core classes)
+
+History 7 - Ms. Reiser
+
+Math 7 - Ms. Jacobsen
+
+English 7 - Ms. Zackharin
+
+Science 7 - Ms. Riggs
+
---
+++ epicPC.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+
+struct epicPC {
+    string cpu;
+    string gpu;
+    string ram;
+    string storage;
+    string os;
+}
+
+int main() {
+    struct epicPC LukesPC = {
+        intel-i7, Nvidia-Geforce-RTX, 32-GB, 2-TB, Windows-10
+    };
+}
